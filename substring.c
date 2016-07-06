6db2dc15a071cccab75ff094dfe8bd488f50f34b
Surjit Sidhu <mail@surjitsidhu.com>
2016-07-06 10:15:54 -0700

    substring

--- substring.c
+++ substring.c
@@ -56,19 +56,14 @@ int main() {
 char *substring(char *string, int position, int length) {
 	char *pointer;
 	int c;
-
-	pointer = malloc(length + 1);
-
+	pointer = (char *) malloc(length + 1);
 	if (pointer == NULL) {
 		printf("Unable to allocate memory.\n");
 		exit(1);
 	}
 
-	for (c = 0; c < length; c++) {
-		*(pointer + c) = *(string + position - 1);
-		string++;
-	}
-
+	for (c = 0; c < length - position; c++)
+		*(pointer + c) = string[position + c];
 	*(pointer + c) = '\0';
 
 	return pointer;
